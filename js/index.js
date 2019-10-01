5c8ebaad172f28be6d2ce4376dafcdaf0f58721a
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-01 17:32:46 -0400

    added wheel and double click events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 
+// scroll event.
 const header = document.querySelector('header');
 let scrollPosition = 0;
 
@@ -13,6 +14,7 @@ document.addEventListener('scroll', (e) => {
     }
 });
 
+// Load event.
 const mainNav = document.querySelector('nav');
 const mainLogo = document.querySelector('.logo-heading');
 
@@ -26,7 +28,7 @@ window.addEventListener('load', (e) => {
 
 // Mouseover and mouseleave event.
 const mainImage = document.querySelector('.intro img');
-console.log(mainImage)
+
 mainImage.addEventListener('mouseover', (e) => {
     e.target.style.transform = 'scale(1.4)';
     e.target.style.transition = 'all 1s';
@@ -34,4 +36,33 @@ mainImage.addEventListener('mouseover', (e) => {
 mainImage.addEventListener('mouseleave', (e) => {
     e.target.style.transform = 'scale(1)';
     e.target.style.transition = 'all 1s';
+});
+
+// wheel event.
+const firstContentImage = document.querySelectorAll('.img-content img')[0];
+let rotatedeg = 0;
+
+firstContentImage.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    let scrollDirection = e.deltaY;
+    if(scrollDirection > 0) {
+        e.target.style.transform = `rotate(${rotatedeg += 1}deg)`;
+    } else {
+        e.target.style.transform = `rotate(${rotatedeg -= 1}deg)`;
+    }
+    
+    console.log('wheel fired');
+});
+
+// double click.
+const firstDestination = document.querySelectorAll('.content-pick .destination')[0]
+const doubleClickBtn = firstDestination.children[2];
+console.log(doubleClickBtn)
+
+doubleClickBtn.addEventListener('dblclick', (e) => {
+    e.target.classList.add('watch-me-bounce');
+});
+doubleClickBtn.addEventListener('animationend', (e) => {
+    console.log('Transition ended');
+    e.target.classList.remove('watch-me-bounce');
 });
