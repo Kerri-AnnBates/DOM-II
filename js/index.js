bc0d0dacac17ae2d10b71156048ef76a1b799a57
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-01 16:11:27 -0400

    implimented load and scroll events to header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const header = document.querySelector('header');
+let scrollPosition = 0;
+
+document.addEventListener('scroll', (e) => {
+    scrollPosition = window.scrollY;
+
+    if(scrollPosition != 0) {
+        header.style.backgroundColor = 'slategrey';
+    } else {
+        header.style.backgroundColor = 'white';
+    }
+});
+
+const mainNav = document.querySelector('nav');
+const mainLogo = document.querySelector('.logo-heading');
+
+window.addEventListener('load', (e) => {
+    mainNav.classList.add('slide-from-right');
+});
+
+window.addEventListener('load', (e) => {
+    mainLogo.classList.add('slide-from-left');
+});
