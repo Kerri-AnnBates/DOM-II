3c95555a0513c1e206bd6a44fed716fe6153ccc8
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-01 19:43:41 -0400

    add event to prevent default for links

--- js/index.js
+++ js/index.js
@@ -102,3 +102,11 @@ document.addEventListener('keydown', (e) => {
 mainLogo.addEventListener('click', (e) => {
     e.target.style.color = '#fdbd52';
 });
+
+// stop defautlt
+const links = document.querySelectorAll('nav a');
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
+});
